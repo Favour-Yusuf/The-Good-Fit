eb67e9733b1bc0f82c71a5b8440711f84c91acba
Favour-Yusuf <favouryusuf45@gmail.com>
2025-08-07 21:33:39 +0100

    phone number

--- src/App.tsx
+++ src/App.tsx
@@ -11,15 +11,15 @@ function App() {
       <Hero
         bgImageUrl="https://res.cloudinary.com/dcuad6p9z/image/upload/v1754426141/hero-fit_x3emdt.jpg"
         logoUrl="https://res.cloudinary.com/dcuad6p9z/image/upload/v1754426028/Thegoodfit-logo_a5zvnk.jpg"
-        whatsappNumber="2348084609951"
+        whatsappNumber="2347033408291"
         gymName="Dee Good Fit"
       />
       {/* ...other sections... */}
       <section id="timetable" className="min-h-[60vh] bg-black text-white">
-        <Timetable whatsappNumber="2348084609951" gymName="Dee Good Fit" />
+        <Timetable whatsappNumber="2347033408291" gymName="Dee Good Fit" />
         {/* timetable goes here */}
       </section>
-        <PrivateTraining whatsappNumber="2348084609951" backgroundImageUrl="https://res.cloudinary.com/dcuad6p9z/image/upload/v1754590973/pexels-pixabay-416717_sva4hu.jpg"/>
+        <PrivateTraining whatsappNumber="2347033408291" backgroundImageUrl="https://res.cloudinary.com/dcuad6p9z/image/upload/v1754590973/pexels-pixabay-416717_sva4hu.jpg"/>
       <Footer/>
     </>
   );
